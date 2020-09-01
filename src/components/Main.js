6dce3a9fa3bf5c00d861a25173cbb113b484828b
Avinash Sontakke <avi1299@gmail.com>
2020-09-01 12:00:39 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -38,7 +38,8 @@ class Main extends React.Component {
           {JSONData.AboutContent.map((item) => {
             return (<div><br/>{item}</div>)
           }
-return(<br>"Thank you for coming to my "<a href="https://www.youtube.com/watch?v=G71WBolYVwU" target="_blank">"TEDx Talk :)"</a>)})
+return(<div><br/>"Thank you for coming to my "<a href="https://www.youtube.com/watch?v=G71WBolYVwU" target="_blank">"TEDx Talk :)"</a></div>
+)})
           {/* </p> */}
           {/* not visible in gatsby build */}
           {close}
